5271ee77c4e07b3c0794a467ab4a91573e859741
Lungelo Mbuyane <nikmbuyane@icloud.com>
2025-04-11 13:34:44 +0200

    apdated math

--- math.java
+++ math.java
@@ -1,12 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int num1 = 5;
-        int num2 = 7;
-
-        int sum = num1 + num2;
-	int num3 = num1 * sum;
-
-        System.out.println("The sum is: " + sum);
-	Sytem,out.println("The multiple of the sum is : " + num3);
+     
+	System.out.println( num1);
     }
 }
