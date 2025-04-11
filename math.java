9e16689ad5108016ef0adcea2834672661018ba4
lindokuhle <bongzlindokuhle@gmail.com>
2025-04-11 12:53:10 +0200

    second commit

--- math.java
+++ math.java
@@ -4,7 +4,9 @@ public class AddTwoNumbers {
         int num2 = 7;
 
         int sum = num1 + num2;
+        int num3= num1 * sum;
 
         System.out.println("The sum is: " + sum);
+        System.out.println("the product of num3 is: "+ num2);
     }
 }
