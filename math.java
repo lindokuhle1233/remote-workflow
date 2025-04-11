5529e1f4e4cdadc51179feda3472e3ccccae1d5d
Lungelo Mbuyane <nikmbuyane@icloud.com>
2025-04-11 12:33:44 +0200

    second commit

--- math.java
+++ math.java
@@ -4,7 +4,9 @@ public class AddTwoNumbers {
         int num2 = 7;
 
         int sum = num1 + num2;
+	int num3 = num1 * sum;
 
         System.out.println("The sum is: " + sum);
+	Sytem,out.println("The multiple of the sum is : " + num3);
     }
 }
